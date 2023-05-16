903a885ab1c4c6c74829f270cea99158c1f2bb91
frank <gamalgamer10@yahoo.com>
2023-05-16 13:43:09 +0300

    project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 
 function App() {
+  const [signture, setSignture] = useState("Name");
+  const [date, setDate] = useState<null | string>(null);
+  const change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setSignture(value.toUpperCase());
+  };
 
   return (
-<div className="bg-black h-[100vh]">
-
-</div>  )
+    <div className=" bg-slate-500 h-[100vh] relative">
+      <div className="font-bold text-4xl flex justify-center flex-col items-center ">
+        <div className="mt-7">{signture}</div>
+        <div className="mt-7">{date}</div>
+      </div>
+      <div className="flex absolute bottom-24 w-full justify-between px-5 ">
+        <input onChange={change} type="text" />
+        <input
+          type="date"
+          onChange={(e): void => {
+            setDate(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
